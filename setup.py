231c7be260e27379f5d88d1aace6072024724d3d
MRZ <33159328+YassinEhab@users.noreply.github.com>
2017-11-25 00:49:38 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+#CODER:MRZ(YASSIN EHAB)
+#FUCKING SITES V.01 GATHERING INFORMATION
+#EGYPT:>
+
+#lets start bb
+# -*- coding: utf-8 -*-
+import os
+MRZ="""
+        ███╗   ███╗██████╗ ███████╗
+        ████╗ ████║██╔══██╗╚══███╔╝
+        ██╔████╔██║██████╔╝  ███╔╝ 
+        ██║╚██╔╝██║██╔══██╗ ███╔╝  
+        ██║ ╚═╝ ██║██║  ██║███████╗
+        ╚═╝#MRZ ╚═╝╚═╝  ╚═╝╚══════╝
+        #SPOOKS,GATHERING&INFORMATION
+   """
+print(MRZ)
+msr=input("ENTER PYTHON PATH: ")
+os.system(msr+"\Scripts\pip install bs4")
+os.system(msr+"\Scripts\pip install requests")
+os.system(msr+"\Scripts\pip install selenium")
